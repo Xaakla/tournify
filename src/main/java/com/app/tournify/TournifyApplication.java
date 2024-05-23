80d48af6b58cabd9b944179262c2ebf38bef7be8
xaakla <diego.rb0307@gmail.com>
2024-05-23 14:24:15 -0300

    initial commit

---
+++ src/main/java/com/app/tournify/TournifyApplication.java
@@ -0,0 +1,13 @@
+package com.app.tournify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/tournify/TournifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tournify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
