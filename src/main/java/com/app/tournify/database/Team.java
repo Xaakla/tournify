5e50c0ab450a7b4e9931451ef69d847e9a39314a
xaakla <diego.rb0307@gmail.com>
2024-05-23 14:57:10 -0300

    adding postgres config and Team entity

---
+++ src/main/java/com/app/tournify/database/Team.java
@@ -0,0 +1,20 @@
+package com.app.tournify.database;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String acronym;
+}
